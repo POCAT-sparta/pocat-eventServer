cca0b5942bbce5d6d7937f0a2a47b03643b2a088
tmdgsudl <leesh27955@gmail.com>
2026-06-12 17:45:21 +0900

    feature : 파라미터 스토어 의존성, 테스트 전용 야물

--- src/test/java/sparta/eventserver/EventServerApplicationTests.java
+++ src/test/java/sparta/eventserver/EventServerApplicationTests.java
@@ -2,8 +2,10 @@ package sparta.eventserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class EventServerApplicationTests {
 
     @Test
